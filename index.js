7cb55147e5812d044c163030c3d697af19c5b402
M-TECH <maziakachukwu@gmail.com>
2025-06-17 12:14:21 +0100

    added some send message functionalities

---
+++ index.js
@@ -0,0 +1,40 @@
+const sendBtn = document.getElementById("sendBtn");
+const chatBox = document.getElementById("chatBox");
+const WhatsAppSendBtn = document.getElementById("WhatsAppSendBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+// const sendBtn = document.getElementById("sendBtn");
+
+sendBtn.addEventListener("click", () => {
+    if(chatBox.style.display === "none") {
+       chatBox.style.display = "flex" 
+       sendBtn.style.display = "none"
+    }else{
+       chatBox.style.display = "" 
+       sendBtn.style.display = ""
+    }
+});
+cancelBtn.addEventListener("click", () => {
+    if(sendBtn.style.display === "none") {
+        sendBtn.style.display = "block" 
+        chatBox.style.display = "none"
+    }else{
+        chatBox.style.display = "" 
+        sendBtn.style.display = ""
+    }
+});
+
+WhatsAppSendBtn.addEventListener("click", () => {
+ const message = document.getElementById("messageBox").value
+ const phone = "2348070870159"
+
+ console.log(message);
+
+ if(message.trim()){
+     const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+      window.open(url, "_blank");
+
+ }else {
+      alert("Please enter a message");
+    }
+ 
+});
