f3467544cf3d62adce976809792d6e48e3309cfc
akachukwumazi <maziakachukwu@gmail.com>
2025-10-15 11:23:33 +0100

    contact wrapp up

--- index.js
+++ index.js
@@ -13,13 +13,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 const submitData = document.getElementById('formData');
+
 submitData.addEventListener('submit', function(event) {
     event.preventDefault();
+
     const formData = new FormData(submitData);
     const data = Object.fromEntries(formData);
-    // console.log(data);
-    
-    alert(data);
+
+ 
+    const phoneNumber = "2348070870159"; 
+
+
+    const message = `Hello, I’m contacting you from your website.\n\nName: ${data.name}\nEmail: ${data.email}\nMessage: ${data.message}`;
+
+    const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+    window.open(whatsappURL, '_blank');
+
     submitData.reset();
 });
 
